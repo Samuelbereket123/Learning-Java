b9cc96cc2eba43060dd0476a3510d828b8ac7d8d
= <=>
2026-04-14 20:10:19 +0300

    inheritance in the chatttt

--- oop/Inheritance/Main.java
+++ oop/Inheritance/Main.java
@@ -9,38 +9,24 @@ And will have an extra method called readSample(). This class will extend from t
 We will have Another class named Magazine and it will have an extra attributes like issueNumber and publisher. 
 And we will have an extra method like narrator and duration. This class will extend from the Parent class.
 */
-class LibraryItem {
-    String title;
-    int id;
-    boolean isAvaliable;
-    
-    LibraryItem(String title, int id) {
-        this.title = title;
-        this.id = id;
-        this.isAvaliable = true;
-    }
+class Animal {
+    String Name;
+    int Age;
+    String Color;
 
-    void borrowItem() {
-        if (isAvaliable) {
-            isAvaliable = false;
-            System.out.println(title + " has been borrowed");
-        } else {
-            System.out.println(title + " is not avaiblable");
-        }
-    }
-
-    void returnItem(){
-        isAvaliable = true;
-        System.out.println(title + " is returned");
-    }
-
-    void DisplayInfo() {
-        System.out.println("Title: " + title + " ID " + id);
+    Animal(String Name, int Age, String Color){
+        this.Name = Name;
+        this.Age = Age;
+        this.Color = Color;
     }
 }
 
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Animal("Roxy", 2, "Brown");
 
-class Book extends LibraryItem {
-    String author;
-    int page;
+        System.out.println("The animal is: " + dog.Color);
+        System.out.println("The animal is: " + dog.Name);
+        System.out.println("The animal is: " + dog.Age);
+    }
 }
